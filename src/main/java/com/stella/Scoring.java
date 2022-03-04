0c3556d321e8971a44707eac321001fbfb3849e8
Stella <panyrzn@gmail.com>
2022-03-04 16:09:06 +0800

    scoringng

---
+++ src/main/java/com/stella/Scoring.java
@@ -0,0 +1,10 @@
+package com.stella;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student jack = new Student();
+        jack.name = "Jack";
+        jack.english = 70;
+        jack.math = 88;
+    }
+}
---
+++ src/main/java/com/stella/Student.java
@@ -0,0 +1,7 @@
+package com.stella;
+
+public class Student {
+    String name;
+    int english;
+    int math;
+}
