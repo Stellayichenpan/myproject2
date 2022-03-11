0957e2e4da0e3bd637d5c24c8fbf67c0a3934b1d
Stellayichenpan <panyezn@gmail.com>
2022-03-11 16:29:52 +0800

    建構特殊方法 最後

--- src/main/java/com/stella/Scoring.java
+++ src/main/java/com/stella/Scoring.java
@@ -2,12 +2,16 @@ package com.stella;
 
 public class Scoring {
     public static void main(String[] args) {
-        Student jack = new Student("Jack");
-       // jack.name = "Jack";
+        Student jack = new Student("Jack", 50, 28);
+       /* jack.name = "Jack";
         jack.english = 70;
-        jack.math = 88;
-        Student hank = new Student("Hank",80,97);
+        jack.math = 88;*/
+        Student hank = new Student("Hank", 80, 97);
+        GraduateStudent jane =
+                new GraduateStudent("Jane", 65, 82,95);
+        jane.name = "Jane";
         jack.print();
         hank.print();
+        jane.print();
     }
 }
